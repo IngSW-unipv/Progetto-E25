fe359ba08e9a362ff6ba963ee19f46a80c8589d5
dadepezzi <66356272+dadepezzi@users.noreply.github.com>
2025-02-14 12:45:39 +0100

    prova

--- Code/src/it/unipv/ingsw/model/puntoDeposito/IPuntoDeposito.java
+++ Code/src/it/unipv/ingsw/model/puntoDeposito/IPuntoDeposito.java
@@ -1,8 +1,12 @@
 package it.unipv.ingsw.model.puntoDeposito;
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 public class IPuntoDeposito {
 	
+=======
+public interface IPuntoDeposito {
+>>>>>>> Stashed changes
 =======
 public interface IPuntoDeposito {
 >>>>>>> Stashed changes

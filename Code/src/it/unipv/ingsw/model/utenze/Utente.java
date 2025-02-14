84a61d980d328c63aaae007958b0f094ab685310
dadepezzi <66356272+dadepezzi@users.noreply.github.com>
2025-02-14 14:02:50 +0100

    prova

--- Code/src/it/unipv/ingsw/model/utenze/Utente.java
+++ Code/src/it/unipv/ingsw/model/utenze/Utente.java
@@ -1,6 +1,7 @@
 package it.unipv.ingsw.model.utenze;
+import it.unipv.ingsw.model.Subject;
 
-public class Utente extends ASuperUser{
+public class Utente extends ASuperUser implements Subject{
 
 
 }

413670fd67b87af53043b9fba7ed69b114663d25
dadepezzi <66356272+dadepezzi@users.noreply.github.com>
2025-02-14 13:32:13 +0100

    tolti commenti stash

--- Code/src/it/unipv/ingsw/model/puntoDeposito/IPuntoDeposito.java
+++ Code/src/it/unipv/ingsw/model/puntoDeposito/IPuntoDeposito.java
@@ -1,14 +1,5 @@
 package it.unipv.ingsw.model.puntoDeposito;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-public class IPuntoDeposito {
-	
-=======
 public interface IPuntoDeposito {
->>>>>>> Stashed changes
-=======
-public interface IPuntoDeposito {
->>>>>>> Stashed changes
 
 }

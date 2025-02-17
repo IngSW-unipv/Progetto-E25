f61a18d4c84b9432356564268505c82e48a7bb3a
ZeinK02 <zeinelabdin.karaouni01@universitadipavia.it>
2025-02-17 15:30:45 +0100

    cancellazione metodo main

--- Code/src/it/unipv/ingsw/model/Execute.java
+++ Code/src/it/unipv/ingsw/model/Execute.java
@@ -4,12 +4,5 @@ import it.unipv.ingsw.model.spedizione.puntoDeposito.Locker;
 
 public class Execute {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Object lockerMap = null;
-		((Locker) lockerMap).put(1, new Locker(1, 10));
-		
-	}
 
 }

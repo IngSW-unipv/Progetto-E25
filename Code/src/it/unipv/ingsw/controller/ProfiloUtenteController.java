579c0471f30cd070b17c620d0438f0be510eb612
dadepezzi <66356272+dadepezzi@users.noreply.github.com>
2025-02-25 17:31:37 +0100

    modifiche profiloUtenteController

--- Code/src/it/unipv/ingsw/controller/ProfiloUtenteController.java
+++ Code/src/it/unipv/ingsw/controller/ProfiloUtenteController.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -32,6 +34,7 @@ import it.unipv.ingsw.view.UtenteView;
 public class ProfiloUtenteController {
 
 	private Utente model;
+	Carrier carrier;
 	private UtenteView view; 
 	private ModificaProfiloView modificaProfiloView;
 	private AvviaSpedizioneView avviaSpedizioneView;
@@ -122,8 +125,9 @@ public class ProfiloUtenteController {
 				manageAction();
 			}
 			private void manageAction() {
-				
+//				System.out.println("-4");
 				itinerarioCarrierView = new ItinerarioCarrierView();
+//				System.out.println("-3");
 				okPrendiInCaricoSpedizioneButton();
 			}
 		};
@@ -133,20 +137,26 @@ public class ProfiloUtenteController {
 	private void okPrendiInCaricoSpedizioneButton() {
 		ActionListener okPrendiInCaricoListener=new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
 				manageAction();
 			}
 			private void manageAction() {
-				MatchingService m = new MatchingService();
-				GestoreSpedizioni gs = new GestoreSpedizioni(m);
+				
 				try {
-					// AVVIA SPEDIZIONE
-					Carrier carrier = (Carrier)model;
-					gs.presaInCaricoSpedizione(carrier, null);
+
+					carrier = new Carrier(model.getMail(), model.getPassword(), model.getNome(), model.getCognome(), model.getNumeroTelefono(), model.getIndirizzoCivico(), model.getDataNascita(), model.getFotoDocumento());
+					
 					itinerarioCarrierView.setVisible(false);
+
 					view.setVisible(false);
-					new ItinerarioCarrierController(new ItinerarioCarrierView(), carrier);
+
+					//new ItinerarioCarrierController(new ItinerarioCarrierView(), carrier);
+
+					new ProfiloUtenteController(model, view);
 				} catch (Exception e) {
+					System.out.println(e.getMessage());
 					JOptionPane.showMessageDialog(itinerarioCarrierView, e.getMessage(), "Errore", JOptionPane.ERROR_MESSAGE);
+					
 				}
 			
 			}

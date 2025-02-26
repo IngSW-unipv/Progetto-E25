da0cc1205cc987c751d519e713203f5bca63c603
dadepezzi <66356272+dadepezzi@users.noreply.github.com>
2025-02-26 22:36:09 +0100

    modifiche ad observer admin

--- Code/src/it/unipv/ingsw/model/utenze/Admin.java
+++ Code/src/it/unipv/ingsw/model/utenze/Admin.java
@@ -1,39 +1,38 @@
 package it.unipv.ingsw.model.utenze;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import it.unipv.ingsw.model.Observer;
 import it.unipv.ingsw.model.spedizione.Spedizione;
 
 public class Admin extends ASuperUser implements Observer<Utente>{
 	private int matricola;
+	private List<Utente> utentiDaValidare;
+	
 	//costruttore
 	public Admin(String mail, String password) {
 		super(mail, password);
+		utentiDaValidare = new ArrayList<>();
 	}
-	
-	//costruttore di default
-	public Admin() {
-	}
-
-	
-	//metodi da implementare
-	public boolean validaAccount() {return true;}
-	//public boolean validaAccount(Utente u1,Utente uAttesa1) {return true;}
-	public boolean disattivaAccount() {return true;}
-//	public void update(Spedizione spedizione) {
-		// TODO Auto-generated method stub
 	
 	@Override
 	public void update(Utente dato) {
-		System.out.println("Aggiornato il profilo dell'utente con email: "+dato.getMail());
-		
+		System.out.println("il profilo dell'utente con email: '"+dato.getMail()+"' è stato modificato");
+		utentiDaValidare.add(dato);
 	}
 	
-	public boolean validaAccount(Utente utente) {
-		return true;
+	public void validaAccount(Utente utente) {
+		utente.setStatoProfilo(true);
 	}
 
 	public boolean disattivaAccount(Utente utente) {
+		utente.setStatoProfilo(false);
 		return true;
+	}
+	
+	public void cancellaAccount(Utente utente) {
+		//dao cancellazione utente;
 	}
 
 	public int getMatricola() {

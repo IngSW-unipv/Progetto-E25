a73b41750a21c8036bf8e45c58da183985d12dc2
Marty-26 <martina.zerba01@universitadipavia.it>
2025-02-26 17:50:30 +0100

    Scrittura della view e DAO delle recensioni

---
+++ Code/src/it/unipv/ingsw/controller/RecensioniController.java
@@ -0,0 +1,13 @@
+package it.unipv.ingsw.controller;
+import it.unipv.ingsw.recensioni.*;
+
+public class RecensioniController {
+	
+	private Recensioni model;
+	private String recensione;
+
+	public RecensioniController() {
+		
+	}
+
+}
